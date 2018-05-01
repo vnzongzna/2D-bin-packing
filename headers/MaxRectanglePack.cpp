4896eb839b5ac85e6c6e4cb374e385ff407a7006
Unknown <siddhishree.shree5@gmail.com>
2018-05-02 00:47:07 +0530

    bssf,blsf and bottom left

--- headers/MaxRectanglePack.cpp
+++ headers/MaxRectanglePack.cpp
@@ -13,8 +13,44 @@ void MaxRectanglePack::Init(int width, int height, bool allowFlip = true){
         usedrectangles.clean();
         freeRectangles.push_back(Rect{0,0,width,height});
 }
-
-void Insert(std::vector<RectSize> &rects, std::vector<Rect> &dst, FreeRectChoiceHeuristic method){
+Rect FindPositionForNewNodeBottomLeft(int width, int height)
+{    
+    int bestY=__INT_MAX__;
+    int bestX=__INT_MAX__;
+ for(int i=0;i<freeRectangles.size();i++ )
+ {
+     if(freeRectangles[i].height>=height && freeRectangles[i].width>=width)
+     {
+         int topsideY=freeRectangles[i].y+height;
+         int rightsideX=freeRectangles[i].x+width;
+         if(topsideY<bestY || topsideY==bestY && rightsideX<bestX)
+         { bestNode.x=freeRectangles[i].x;
+         bestNode.y=freeRectangles[i].y;
+         bestNode.width=width;
+         bestNode.height=height;
+         bestY=topsideY;
+         bestX=rightsideX;
+         }
+    }
+    if(binAllowFlip && freeRectangles[i].height>=width && freeRectangles[i].width>=height)
+    {
+        int topsideY=freeRectangles[i].y+width;
+        int rightsideX=freeRectangles[i].x+height;
+        if(topsideY<bestY || topsideY==bestY && rightsideX<bestX)
+        {
+            bestNode.x=freeRectangles[i].x;
+            bestNode.y=freeRectangles[i].y;
+            bestNode.width=width;
+            bestNode.height=height;
+            bestY=topsideY;
+            bestX=rightsideX;
+        }
+    }
+ }
+ return bestNode;
+}
+void Insert(std::vector<RectSize> &rects, std::vector<Rect> &dst, FreeRectChoiceHeuristic method)
+{
         /*  Something To Be Done Here   */
 }
 
