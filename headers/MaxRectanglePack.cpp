a6d2634d416ede76b59e1d90bb601abe80b5f2fd
prakhar-s <prakhardevsingh4@gmail.com>
2018-05-02 01:30:38 +0530

    Area and Perimeter are verified(#3)

--- headers/MaxRectanglePack.cpp
+++ headers/MaxRectanglePack.cpp
@@ -14,6 +14,122 @@ void MaxRectanglePack::Init(int width, int height, bool allowFlip = true){
         freeRectangles.push_back(Rect{0,0,width,height});
 }
 
+
+ Rect MaxRectsBinPack::FindPositionForNewNodeBestAreaFit(int width, int height,
+	int &bestAreaFit, int &bestShortSideFit) const
+{
+	Rect bestNode;
+	memset(&bestNode, 0, sizeof(Rect));
+
+	bestAreaFit = std::numeric_limits<int>::max();
+	bestShortSideFit = std::numeric_limits<int>::max();
+
+	for(size_t i = 0; i < freeRectangles.size(); ++i)
+	{
+		//int areaFit = freeRectangles[i].width * freeRectangles[i].height - width * height;
+          int areaFit = freeRectangles[i].width * freeRectangles[i].height;
+		// Try to place the rectangle in upright (non-flipped) orientation.
+		if (freeRectangles[i].width >= width && freeRectangles[i].height >= height)
+		{
+			int leftoverHoriz = abs(freeRectangles[i].width - width);
+			int leftoverVert = abs(freeRectangles[i].height - height);
+			int shortSideFit = min(leftoverHoriz, leftoverVert);
+
+			if (areaFit < bestAreaFit || (areaFit == bestAreaFit && shortSideFit < bestShortSideFit))
+			{
+				bestNode.x = freeRectangles[i].x;
+				bestNode.y = freeRectangles[i].y;
+				bestNode.width = width;
+				bestNode.height = height;
+				bestShortSideFit = shortSideFit;
+				bestAreaFit = areaFit;
+			}
+		}
+
+		if (binAllowFlip && freeRectangles[i].width >= height && freeRectangles[i].height >= width)
+		{
+			int leftoverHoriz = abs(freeRectangles[i].width - height);
+			int leftoverVert = abs(freeRectangles[i].height - width);
+			int shortSideFit = min(leftoverHoriz, leftoverVert);
+
+			if (areaFit < bestAreaFit || (areaFit == bestAreaFit && shortSideFit < bestShortSideFit))
+			{
+				bestNode.x = freeRectangles[i].x;
+				bestNode.y = freeRectangles[i].y;
+				bestNode.width = height;
+				bestNode.height = width;
+				bestShortSideFit = shortSideFit;
+				bestAreaFit = areaFit;
+			}
+		}
+	}
+	return bestNode;
+}
+
+/// Returns 0 if the two intervals i1 and i2 are disjoint, or the length of their overlap otherwise.
+int CommonIntervalLength(int i1start, int i1end, int i2start, int i2end)
+{
+	if (i1end < i2start || i2end < i1start)
+		return 0;
+	return min(i1end, i2end) - max(i1start, i2start);
+}
+
+int MaxRectsBinPack::ContactPointScoreNode(int x, int y, int width, int height) const
+{
+	int score = 0;
+
+	if (x == 0 || x + width == binWidth)
+		score += height;
+	if (y == 0 || y + height == binHeight)
+		score += width;
+
+	for(size_t i = 0; i < usedRectangles.size(); ++i)
+	{
+		if (usedRectangles[i].x == x + width || usedRectangles[i].x + usedRectangles[i].width == x)
+			score += CommonIntervalLength(usedRectangles[i].y, usedRectangles[i].y + usedRectangles[i].height, y, y + height);
+		if (usedRectangles[i].y == y + height || usedRectangles[i].y + usedRectangles[i].height == y)
+			score += CommonIntervalLength(usedRectangles[i].x, usedRectangles[i].x + usedRectangles[i].width, x, x + width);
+	}
+	return score;
+}
+
+Rect MaxRectsBinPack::FindPositionForNewNodeContactPoint(int width, int height, int &bestContactScore) const
+{
+	Rect bestNode;
+	memset(&bestNode, 0, sizeof(Rect));
+
+	bestContactScore = -1;
+
+	for(size_t i = 0; i < freeRectangles.size(); ++i)
+	{
+		// Try to place the rectangle in upright (non-flipped) orientation.
+		if (freeRectangles[i].width >= width && freeRectangles[i].height >= height)
+		{
+			int score = ContactPointScoreNode(freeRectangles[i].x, freeRectangles[i].y, width, height);
+			if (score > bestContactScore)
+			{
+				bestNode.x = freeRectangles[i].x;
+				bestNode.y = freeRectangles[i].y;
+				bestNode.width = width;
+				bestNode.height = height;
+				bestContactScore = score;
+			}
+		}
+		if (freeRectangles[i].width >= height && freeRectangles[i].height >= width)
+		{
+			int score = ContactPointScoreNode(freeRectangles[i].x, freeRectangles[i].y, height, width);
+			if (score > bestContactScore)
+			{
+				bestNode.x = freeRectangles[i].x;
+				bestNode.y = freeRectangles[i].y;
+				bestNode.width = height;
+				bestNode.height = width;
+				bestContactScore = score;
+			}
+		}
+	}
+	return bestNode;
+=======
 Rect MaxRectsBinPack::Insert(int width, int height, FreeRectChoiceHeuristic method)
         Rect temp;
         switch(method){
@@ -36,6 +152,7 @@ Rect MaxRectsBinPack::Insert(int width, int height, FreeRectChoiceHeuristic meth
         FixFreeList();
         usedrectangles.push_back(Node);
         return Node;
+
 }
 
 Rect FindPositionForNewNodeBottomLeft(int width, int height)
